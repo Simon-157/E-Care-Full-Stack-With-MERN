7ee9cea2bde5258c3f9579e11e82e9c6c6add2da
Simon-157 <boatengsimonjnr157@gmail.com>
2022-03-13 11:52:48 +0000

    added auth

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const cookieSession = require("cookie-session");
+const express = require("express");
+const cors = require("cors");
+const passportSetup = require("./passport");
+const passport = require("passport");
+const authRoute = require("./routes/auth");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(
+  cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+app.use("/auth", authRoute);
+
+app.listen(port, () =>
+  console.log(`server running on PORT ${port} 🚀`)
+);
--- server/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const authenticateUser = require('../controllers/auth.cont')
-
-
-const router = express.Router();
-
-router.post('/api/google-login', authenticateUser);
-
-module.exports = router;
